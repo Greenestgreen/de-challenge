97f75d57bd53320e90527e97a94e7ee24b4de8af
Roberto Matus <regular.greener.grass@hotmail.com>
2022-01-12 17:10:58 -0300

    improvements

--- src/utils/clean_result.py
+++ src/utils/clean_result.py
@@ -1,15 +1,16 @@
 
-from pyspark.sql.functions import current_date, date_format, to_date, when
+from pyspark.sql.functions import to_date, when
 
 
 class CleanResult():
 
     
 
-    
+    #This cleans the userscore field changing 'tbd' value to -1 and then casting it to float
     def clean_tbd(result):               
         return result.withColumn('userscore', when(result.userscore == 'tbd', '-1').otherwise(result.userscore).cast('float'))
 
+    #This exchange the date display to YYYY-MM-DD and casting it to date 
     def cast_date(result):
         return result.withColumn('date', to_date(result.date,'LLL d, yyyy'))
 
